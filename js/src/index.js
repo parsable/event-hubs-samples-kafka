730b1e7ac7751b08a843cea9b5b44b576a84a37c
sergeylemekhov <sergey.lemekhov@parsable.com>
2021-07-01 14:50:14 +0300

    ANLZ-1566 use single file

--- js/src/index.js
+++ js/src/index.js
@@ -1,6 +1,107 @@
 const {Command, flags} = require('@oclif/command');
-const DataFeedConsumer = require('consumer');
-const {cli} = require('cli-ux')
+const {cli} = require('cli-ux');
+const msal = require('@azure/msal-node');
+const {Kafka} = require('kafkajs');
+
+class DataFeedConsumer {
+    namespace;
+    topic;
+    tenantId;
+    clientId;
+    clientSecret;
+    tokenRequest;
+
+    constructor(namespace, topic, tenantId, clientId, clientSecret, group) {
+        this.namespace = namespace;
+        this.topic = topic;
+        this.tenantId = tenantId;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.tokenRequest = {
+            scopes: ['https://' + this.namespace + '.servicebus.windows.net/.default'],
+        };
+        this.group = group ? group : 'data-feed-cli-consumer';
+    }
+
+    createKafka() {
+        return new Kafka({
+            clientId: 'test-js-consumer',
+            brokers: [this.namespace + '.servicebus.windows.net:9093'],
+            // authenticationTimeout: 1000,
+            // reauthenticationThreshold: 10000,
+            ssl: true,
+            sasl: {
+                mechanism: 'oauthbearer',
+                oauthBearerProvider: async () => {
+                    let authenticationResult = await this.azureAuth();
+                    return {
+                        value: authenticationResult.accessToken
+                    };
+                }
+            },
+        });
+    }
+
+    createConsumer() {
+        const kafka = this.createKafka();
+        return kafka.consumer({groupId: this.group});
+    }
+
+    async azureAuth() {
+        let token = await this.cca.acquireTokenByClientCredential(this.tokenRequest);
+        console.log("Azure token acquired")
+        return token;
+    }
+
+    async start() {
+        this.validate();
+        this.initClientApp();
+
+        const consumer = this.createConsumer();
+        await consumer.connect();
+        await consumer.subscribe({topic: this.topic});
+
+        await consumer.run({
+            eachMessage: async ({topic, partition, message}) => {
+                console.log({
+                    key: message.key,
+                    value: message.value.toString(),
+                    headers: message.headers,
+                });
+            },
+        });
+    }
+
+    validate() {
+        if (!this.namespace) {
+            throw `Error: Empty namespace.`;
+        }
+        if (!this.topic) {
+            throw `Error: Empty topic.`;
+        }
+        if (!this.tenantId) {
+            throw `Error: Empty tenant id.`;
+        }
+        if (!this.clientId) {
+            throw `Error: Empty clientId.`;
+        }
+        if (!this.clientSecret) {
+            throw `Error: Empty client secret.`;
+        }
+    }
+
+    initClientApp() {
+        let msalConfig = {
+            auth: {
+                authority: 'https://login.microsoftonline.com/' + this.tenantId,
+                clientId: this.clientId,
+                clientSecret: this.clientSecret,
+            }
+        };
+        this.cca = new msal.ConfidentialClientApplication(msalConfig);
+    }
+}
+
 
 class ConsumeCommand extends Command {
     async run() {
