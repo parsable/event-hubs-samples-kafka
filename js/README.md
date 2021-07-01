eeb4efb45db03ec1f7419ef3c1602cbd5d7c2623
Sergey Lemekhov <75332438+slemekhov@users.noreply.github.com>
2021-07-01 15:40:24 +0300

    Anlz-1566 consumer app

--- js/README.md
+++ js/README.md
@@ -1,41 +1,22 @@
-# Receive Messages in Node using Event Hubs for Apache Kafka Ecosystems
+# Data Feed Consumer
 
-This quickstart will show how to create and connect to an Event Hubs Kafka endpoint using an example producer and consumer written in Node. Azure Event Hubs for Apache Kafka Ecosystems supports [Apache Kafka version 1.0](https://kafka.apache.org/10/documentation.html) and later.
+A simple consumer for Parsable Data Feed.
 
-This sample uses the [KafkaJS](https://github.com/tulios/kafkajs) library.
+This consumer uses the [KafkaJS](https://github.com/tulios/kafkajs) library.
 
 ## Prerequisites
+
 - [npm](https://www.npmjs.com/)
 - [Node.js](https://nodejs.org)
 
-## Configuration
+## Usage
 
-Replace placeholders with your Event Hubs namespace, client id, client secret, and tenant id:
-
-```javascript
-const namespace = "<REPLACE WITH YOUR EVENT HUBS NAMESPACE>";
-const tenantId = "<REPLACE WITH YOUR TENANT ID>";
-const msalConfig = {
-    auth: {
-        authority: "https://login.microsoftonline.com/" + tenantId,
-        clientId: "<REPLACE WITH YOUR CLIENT ID>",
-        clientSecret: "<REPLACE WITH YOUR CLIENT SECRET>",
-    }
-};
-
+```sh-session
+$ npm install -g @wearableintelligence/data-feed-consumer
+$ datafeedconsumer -n namespace -h hubName -t tenantId -c clientId -s clientSecret
+Starting consumer...
+$ datafeedconsumer --help
+USAGE
+  $ datafeedconsumer COMMAND
+...
 ```
-
-## Run consumer
-
-The consumer sample demonstrates how to receive messages from the Event Hubs service using the Kafka API.
-
-1. install dependencies with running
-    ```bash
-    npm install
-    ```
-1. start consumer with
-    ```bash
-    node consumer.js
-    ```
-
-The consumer will now begin receiving events from the Kafka enabled Event Hub on topic `test`. Change the topic variable in `consumer.js` for a different one. If the topic has not already been created in the Kafka configuration, no messages will be sent/received.
---
+++ js/src/index.js
@@ -0,0 +1,145 @@
+const {Command, flags} = require('@oclif/command');
+const {cli} = require('cli-ux');
+const msal = require('@azure/msal-node');
+const {Kafka} = require('kafkajs');
+
+class DataFeedConsumer {
+    namespace;
+    topic;
+    tenantId;
+    clientId;
+    clientSecret;
+    tokenRequest;
+
+    constructor(namespace, topic, tenantId, clientId, clientSecret, group) {
+        this.namespace = namespace;
+        this.topic = topic;
+        this.tenantId = tenantId;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.tokenRequest = {
+            scopes: ['https://' + this.namespace + '.servicebus.windows.net/.default'],
+        };
+        this.group = group ? group : 'data-feed-cli-consumer';
+    }
+
+    createKafka() {
+        return new Kafka({
+            clientId: 'test-js-consumer',
+            brokers: [this.namespace + '.servicebus.windows.net:9093'],
+            // authenticationTimeout: 1000,
+            // reauthenticationThreshold: 10000,
+            ssl: true,
+            sasl: {
+                mechanism: 'oauthbearer',
+                oauthBearerProvider: async () => {
+                    let authenticationResult = await this.azureAuth();
+                    return {
+                        value: authenticationResult.accessToken
+                    };
+                }
+            },
+        });
+    }
+
+    createConsumer() {
+        const kafka = this.createKafka();
+        return kafka.consumer({groupId: this.group});
+    }
+
+    async azureAuth() {
+        let token = await this.cca.acquireTokenByClientCredential(this.tokenRequest);
+        console.log("Azure token acquired")
+        return token;
+    }
+
+    async start() {
+        this.validate();
+        this.initClientApp();
+
+        const consumer = this.createConsumer();
+        await consumer.connect();
+        await consumer.subscribe({topic: this.topic});
+
+        await consumer.run({
+            eachMessage: async ({topic, partition, message}) => {
+                console.log({
+                    key: message.key,
+                    value: message.value.toString(),
+                    headers: message.headers,
+                });
+            },
+        });
+    }
+
+    validate() {
+        if (!this.namespace) {
+            throw `Error: Empty namespace.`;
+        }
+        if (!this.topic) {
+            throw `Error: Empty topic.`;
+        }
+        if (!this.tenantId) {
+            throw `Error: Empty tenant id.`;
+        }
+        if (!this.clientId) {
+            throw `Error: Empty clientId.`;
+        }
+        if (!this.clientSecret) {
+            throw `Error: Empty client secret.`;
+        }
+    }
+
+    initClientApp() {
+        let msalConfig = {
+            auth: {
+                authority: 'https://login.microsoftonline.com/' + this.tenantId,
+                clientId: this.clientId,
+                clientSecret: this.clientSecret,
+            }
+        };
+        this.cca = new msal.ConfidentialClientApplication(msalConfig);
+    }
+}
+
+
+class ConsumeCommand extends Command {
+    async run() {
+        const {flags} = this.parse(ConsumeCommand);
+        const dataFeedConsumer = new DataFeedConsumer(
+            flags.namespace,
+            flags.hub,
+            flags.tenant,
+            flags.client,
+            flags.secret
+        );
+
+        try {
+            cli.action.start('Starting consumer');
+            await dataFeedConsumer.start();
+            cli.action.stop();
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+ConsumeCommand.description = `Data Feed Consumer
+A simple consumer which allows performing sanity checks. It connects to an Event Hub and listens for new messages.
+Messages logged to the console.
+`
+
+ConsumeCommand.flags = {
+    namespace: flags.string({char: 'n', description: 'Event Hubs namespace', required: true}),
+    hub: flags.string({char: 'h', description: 'Event hub name', required: true}),
+    tenant: flags.string({char: 't', description: 'Tenant ID', required: true}),
+    client: flags.string({char: 'c', description: 'Client ID', required: true}),
+    secret: flags.string({char: 's', description: 'Client secret', required: true}),
+    group: flags.string({char: 'g', description: 'Consumer group', default: 'data-feed-cli-consumer'}),
+}
+
+ConsumeCommand.examples = [
+    'datafeedconsumer -n my-eventhubs-namespace -h my-hub-name -t 793f32ca-fc7a-499a-ae14-eb5adee63165 -c 8742ab2d-6652-4d1b-a946-ed571f2bd39a -s my-client-secret'
+];
+
+module.exports = ConsumeCommand
