37e67c53c45d0af9ff31ada1526ba098adde607e
sergeylemekhov <sergey.lemekhov@parsable.com>
2021-08-03 17:17:36 +0300

    ANLZ-1736 read from the beginning

--- python/settings.py
+++ python/settings.py
@@ -9,3 +9,6 @@ TOPIC = os.getenv("TOPIC")
 TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CONSUMER_GROUP = os.getenv("CONSUMER_GROUP", "test-consumer-python")
+FROM_BEGINNING = os.getenv("FROM_BEGINNING", "false").lower() == 'true'
+
